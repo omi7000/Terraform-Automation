177438109aa5a1d10af0ff8cebe503004ca3e401
omi7000 <124768917+omi7000@users.noreply.github.com>
2023-05-30 14:00:38 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "mydev-tf-state-bucket-7"
+    bucket = "my-terrafrom-project"
     key = "main"
     region = "ap-south-1"
     dynamodb_table = "my-dynamodb-table"
